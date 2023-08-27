d26053af682ad71c3f92acefa070509781f87024
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2023-08-27 15:19:21 +0300

    removed trial controller methods and endpoints

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\News\GuardianFetcher;
-use App\Services\News\NewsapiFetcher;
-use App\Services\News\NYTFetcher;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -12,25 +9,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    public function guardian()
-    {
-        return 'guardian';
-//        $api = new GuardianFetcher(1, 2);
-//        $api->execute();
-    }
-
-    public function nyt()
-    {
-        return 'nyt';
-//        $api = new NYTFetcher(1, 2);
-//        $api->execute();
-    }
-
-    public function newsapi()
-    {
-        return 'newsapi';
-//        $api = new NewsapiFetcher(1, 2);
-//        $api->execute();
-    }
 }
