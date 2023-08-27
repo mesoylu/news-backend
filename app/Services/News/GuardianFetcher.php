c3606daeceb1052d803640ca22cd366fab01ed49
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2023-08-27 18:41:58 +0300

    implemented saveToDB on GuardianFetcher

--- app/Services/News/GuardianFetcher.php
+++ app/Services/News/GuardianFetcher.php
@@ -3,6 +3,11 @@
 namespace App\Services\News;
 
 use App\Exceptions\NewsFetchException;
+use App\Models\Article;
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\Source;
+use Illuminate\Database\UniqueConstraintViolationException;
 use Throwable;
 
 class GuardianFetcher extends AbstractFetcher
@@ -46,6 +51,79 @@ class GuardianFetcher extends AbstractFetcher
 
     private function saveToDB(array $articles)
     {
-        // do something
+        try {
+            foreach ($articles as $article) {
+                try {
+                    $authors = $this->parseAuthors($article['tags']);
+                    $categories = $this->parseCategories($article['tags']);
+
+                    $source = Source::firstOrCreate([
+                        'name' => 'The Guardian',
+                        'source_id' => 'guardian',
+                        'api_name' => 'guardian',
+                    ]);
+
+                    $createdArticle = Article::create([
+                        'source_id' => $source->id,
+                        'title' => $article['webTitle'],
+                        'description' => $article['fields']['trailText'] ?? '',
+                        'article_url' => $article['webUrl'],
+                        'image_url' => $article['fields']['thumbnail'] ?? '',
+                        'published_at' => date('Y-m-d H:i:s', strtotime($article['webPublicationDate'])),
+                    ]);
+
+                    $authorIds = [];
+                    foreach ($authors as $authorName) {
+                        $author = Author::firstOrCreate(['name' => $authorName]);
+                        $authorIds[] = $author->id;
+                    }
+                    $createdArticle->authors()->sync($authorIds);
+
+                    $categoryIds = [];
+                    foreach ($categories as $categoryName) {
+                        $category = Category::firstOrCreate(['name' => $categoryName]);
+                        $categoryIds[] = $category->id;
+                    }
+                    $createdArticle->categories()->sync($categoryIds);
+                } catch (UniqueConstraintViolationException $t) {
+                    continue;
+                } catch (Throwable $t) {
+                    report($t);
+                    continue;
+                }
+            }
+        } catch (Throwable $e) {
+            report($e);
+        }
+    }
+
+    private function parseAuthors(?array $tags): array
+    {
+        try {
+            $authors = array();
+            foreach ($tags as $tag) {
+                if (array_key_exists('type', $tag) && $tag['type'] == 'contributor') {
+                    $authors[] = $tag['webTitle'] ?? null;
+                }
+            }
+            return array_filter($authors);
+        } catch (Throwable) {
+            return array();
+        }
+    }
+
+    private function parseCategories(?array $tags): array
+    {
+        try {
+            $categories = array();
+            foreach ($tags as $tag) {
+                if (array_key_exists('type', $tag) && $tag['type'] == 'keyword') {
+                    $categories[] = $tag['webTitle'] ?? null;
+                }
+            }
+            return array_filter($categories);
+        } catch (Throwable) {
+            return array();
+        }
     }
 }
