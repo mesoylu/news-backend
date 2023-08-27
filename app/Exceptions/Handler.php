3501c3d8d44a915ccb9778b48c8c2b943be2727a
Mehmet Eren Soylu <eren.soylu@dsg.com.tr>
2023-08-27 18:06:09 +0300

    extended report method to be more readable on laravel log

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +27,33 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Report or log an exception.
+     *
+     * @param Throwable $exception
+     * @return void
+     * @throws Exception|Throwable
+     */
+    public function report(Throwable $exception)
+    {
+        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+            $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } elseif (array_key_exists('REMOTE_ADDR', $_SERVER)) {
+            $ipAddress = $_SERVER['REMOTE_ADDR'];
+        } else {
+            $ipAddress = '127.0.0.1';
+        }
+        Log::error(
+            sprintf(
+                "%s: %s in %s:%d %s",
+                get_class($exception),
+                $exception->getMessage(),
+                $exception->getFile(),
+                $exception->getLine(),
+                $ipAddress
+            )
+        );
     }
 }
